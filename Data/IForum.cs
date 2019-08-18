b14ca4554265df95fc162c0f20be59a9d9ee45a2
Sanchos <monabrwork@gmail.com>
2019-08-18 22:32:13 +0300

    feat: methods for forum model

---
+++ Data/IForum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Data
+{
+    public interface IForum
+    {
+        ForumModel GetById(int id);
+        IEnumerable<ForumModel> GetAll();
+        IEnumerable<ApplicationUser> ApplicationUsers();
+
+        Task Create(ForumModel forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
---
+++ Service/ForumService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Service
+{
+    public class ForumService:IForum
+    {
+        private readonly ApplicationDbContext _context;
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public ForumModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ForumModel> GetAll()
+        {
+            return _context.Forums
+                .Include(forum=>forum.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> ApplicationUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(ForumModel forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
