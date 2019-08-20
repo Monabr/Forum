9b0d671692362ccbf7e76f17d284f62ba77ef4ac
Sanchos <monabrwork@gmail.com>
2019-08-20 03:45:06 +0300

    Стиль для тем и ответов

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Forum.Data;
+using Forum.Models;
+using Forum.Models.Forum;
+using Forum.Models.Post;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Forum.Models;
 
 namespace Forum.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+
+        //private HomeIndexModel BuildHomeIndexModel()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
         public IActionResult Privacy()
         {
             return View();
         }
 
+        private readonly IForum _forumService;
+        private readonly IPost _postService;
+
+        public HomeController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll()
+                .Select(forum => new ForumListingModel
+                {
+                    Id = forum.Id,
+                    Name = forum.Title,
+                    Description = forum.Description
+                });
+
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Topic(int id)
+        {
+            var forum = _forumService.GetById(id);
+            var posts = forum.Posts;
+
+            var postListings = posts.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                AuthorId = post.User.Id,
+                AuthorName = post.User.UserName,
+                Title = post.Title,
+                DatePosted = post.Created.ToString(),
+                RepliesCount = post.Replies.Count(),
+                Forum = BuildForumListing(post)
+            });
+
+            var model = new ForumTopicModel
+            {
+                Posts = postListings,
+                Forum = BuildForumListing(forum)
+            };
+
+            return View(model);
+        }
+
+        private ForumListingModel BuildForumListing(Post post)
+        {
+            var forum = post.ForumModel;
+            return BuildForumListing(forum);
+        }
+
+        private ForumListingModel BuildForumListing(ForumModel forum)
+        {
+            return new ForumListingModel
+            {
+                Id = forum.Id,
+                Name = forum.Title,
+                Description = forum.Description
+            };
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
+
+
+
+
